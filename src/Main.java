4e0dc4ac113ad2998e4b072676aba60103260b05
Ann Chapyrkina <chapyrkinaa@mail.ru>
2023-04-21 17:13:53 +0400

    Transferring methods

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,67 @@
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
 
 public class Main {
+
+    private static StringBuilder builder = new StringBuilder();
+
     public static void main(String[] args) {
 
-        CreateDirectory createDirectory = new CreateDirectory();
-        createDirectory.addDirectory("D://Games//src");
-        createDirectory.addDirectory("D://Games//res");
-        createDirectory.addDirectory("D://Games//savegames");
-        createDirectory.addDirectory("D://Games//temp");
-        createDirectory.addDirectory("D://Games//src//main");
-        createDirectory.addDirectory("D://Games//src//test");
+        createDirectory("D://Games//src");
+        createDirectory("D://Games//res");
+        createDirectory("D://Games//savegames");
+        createDirectory("D://Games//temp");
+        createDirectory("D://Games//src//main");
+        createDirectory("D://Games//src//test");
 
-        CreateFile createFile = new CreateFile();
-        createFile.addFile("D://Games//src//main//Main.java");
-        createFile.addFile("D://Games//src//main//Utils.java");
+        createFile("D://Games//src//main", "Main.java");
+        createFile("D://Games//src//main", "Utils.java");
 
-        createDirectory.addDirectory("D://Games//res//drawables");
-        createDirectory.addDirectory("D://Games//res//vectors");
-        createDirectory.addDirectory("D://Games//res//icons");
+        createDirectory("D://Games//res//drawables");
+        createDirectory("D://Games//res//vectors");
+        createDirectory("D://Games//res//icons");
 
-        createFile.addFile("D://Games//temp//temp.txt");
+        createFile("D://Games//temp", "temp.txt");
 
         try (FileWriter log = new FileWriter("D://Games//temp//temp.txt", false)) {
-            log.write(createFile.getBuilder().toString());
-            log.write(createDirectory.getBuilder().toString());
+            log.write(builder.toString());
             log.flush();
         } catch (IOException ex) {
             System.out.println("Ошибка записи лога");
+        }
+    }
+
+    public static void createDirectory(String name) {
+
+        File dir = new File(name);
+        Date date = new Date();
+        if (dir.mkdir())
+            builder.append("\nПапка " + dir.getName() + " создана" + "\n"
+                    + "в папке " + dir.getParent() + "\n"
+                    + "путь: " + dir.getAbsolutePath() + "\n"
+                    + "время: " + date + "\n");
+        else {
+            builder.append("\nПапку " + name + " добавить не удалось" + "\n"
+                    + "время: " + date + "\n");
+        }
+    }
+
+    public static void createFile(String path, String name) {
+
+        File myFile = new File(path + "//" + name);
+        Date date = new Date();
+
+        try {
+            if (myFile.createNewFile())
+                builder.append("\nФайл " + myFile.getName() + " добавлен" + "\n"
+                        + "в папку " + myFile.getParentFile() + "\n"
+                        + "путь: " + myFile + "\n"
+                        + "время: " + date + "\n");
+        } catch (IOException ex) {
+            builder.append("\nФайл " + name + " добавить не удалось" + "\n"
+                    + "время: " + date + "\n");
         }
     }
 }
